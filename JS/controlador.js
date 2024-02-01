e9a79a57e121f98c20a83b8b13fe2b12c9958ae9
EliasEJ <e.jerari@sapalomera.cat>
2024-02-01 16:56:34 +0100

    Refactor code for file upload and JSON parsing

--- JS/controlador.js
+++ JS/controlador.js
@@ -1,19 +1,34 @@
 "use strict";
 
-function init(){
+function init() {
     recuperar();
 }
 
-function recuperar(){
-    $("#recuperar").on("click",function(){
-        var input = document.createElement('input');
+function recuperar() {
+    $("#recuperar").on("click", function () {
+        let input = document.createElement('input');
         input.type = 'file';
         //validar si es un archivos .json
         input.accept = '.json';
-        input.onchange = e => { 
-           var file = e.target.files[0]; 
-           // Aquí puedes manejar el archivo seleccionado
-           
+        input.onchange = e => {
+            let file = e.target.files[0];
+            // Aquí puedes manejar el archivo seleccionado
+            let reader = new FileReader();
+            reader.onload = function(event) {
+                // Parsear el archivo JSON
+                let obj = JSON.parse(event.target.result);
+                let output = '';
+                // Iterar sobre los datos del objeto
+                for (let key in obj) {
+                    // Ignorar la propiedad 'articles'
+                    if (key !== 'articles') {
+                        output += `<tr><td>${key}</td><td>${obj[key]}</td></tr>`;
+                    }
+                }
+                // Agregar los datos al tbody
+                $('#taula').html(output);
+            };
+            reader.readAsText(file);
         }
         input.click();
     });
