7b4a079b47d5bdbc9d7a2066595a8d352138671e
atarensiSAPA <a.tarensi2@sapalomera.cat>
2024-02-01 16:36:17 +0100

    bo

--- JS/controlador.js
+++ JS/controlador.js
@@ -1,17 +1,22 @@
 "use strict";
 
 function init(){
-
+    recuperar();
 }
 
 function recuperar(){
     $("#recuperar").on("click",function(){
         var input = document.createElement('input');
         input.type = 'file';
+        //validar si es un archivos .json
+        input.accept = '.json';
         input.onchange = e => { 
            var file = e.target.files[0]; 
            // Aquí puedes manejar el archivo seleccionado
+           
         }
         input.click();
     });
 }
+
+$(init);
