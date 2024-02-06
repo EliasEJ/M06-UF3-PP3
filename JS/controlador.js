e049135b93debfc3ee897e913af61f2a2a558fb6
atarensiSAPA <a.tarensi2@sapalomera.cat>
2024-02-06 21:03:26 +0100

    factura

--- JS/controlador.js
+++ JS/controlador.js
@@ -3,13 +3,20 @@
 let dialog = document.getElementById("novaFactura");
 let dialogEditar = document.getElementById("editarFactura");
 let style = {style: "currency", currency: "EUR"};
-
-let numFactura = $("#numFactura").val() !== "" ? $("#numFactura").val() : 0;
+let numFactura;
 
 function init() {
     recuperar();
     obrirNovaFactura();
     tancarFactura();
+    //insertar el numero de factura en el campo de la factura nueva mientras tiene en cuenta el ultimo numero de factura en la tabla
+
+    if($("#taula tr:last-child td:first-child").text() == ""){
+        numFactura = 1;
+    }else {
+        numFactura = parseInt($("#taula tr:last-child td:first-child").text()) + 1;
+    }
+    $("#numFactura").val(numFactura);
 }
 
 function recuperar() {
@@ -26,13 +33,13 @@ function recuperar() {
             reader.onload = function(event) {
                 // Parsear el archivo JSON
                 let factures = JSON.parse(event.target.result);
-                //if(factures.pagada == true) ? "Si" : "No";
+                
                 for (let f in factures){
                     for (let i in factures[f]){
                         let factura = factures[f][i];
                         let fila = `
                             <tr id="fila-${factura.id}">
-                                <td>${factura.id}</td>
+                                <td id="numFacturaTaula_${factura.id}">${factura.id}</td>
                                 <td>${factura.data}</td>
                                 <td>${factura.nif}</td>
                                 <td>${factura.nom_client}</td>
@@ -43,7 +50,7 @@ function recuperar() {
                                 <td>${factura.baseI.toLocaleString("es-ES", style)}</td>
                                 <td>${factura.iva}</td>
                                 <td>${factura.total.toLocaleString("es-ES", style)}</td>
-                                <td>${factura.pagada}</td>
+                                <td>${factures.pagada ? "Si" : "No"}</td>
                                 <td>
                                 <img src="IMG/eliminar.png" onclick="eliminarFactura('fila-${factura.id}')" alt="Eliminar">
                                 <img src="IMG/imprimir.png" onclick="imprimirFactura(${factura.id})" alt="Imprimir">
@@ -128,8 +135,8 @@ $("#guardarFactura").on("click", function(){
     if ($("#numFactura").val() != "" && $("#data").val() != "" && $("#email").val().match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/) && $("#nif").val().match(/[0-9]{8}[A-Za-z]{1}/) && $("#nom").val().match(/[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]+/) && $("#telefon").val().match(/[0-9]{9}/) && $("#dte").val().match(/[0-9]{2}/) && $("#iva").val().match(/[0-9]{2}/)) {
         //Imprimir factura en la tabla de facturas
         let fila = `
-        <tr id="fila-${$("#numFactura").val()}">
-            <td>${$("#numFactura").val()}</td>
+        <tr id="fila-${$("#numFactura").val(numFactura)}">
+            <td id="numFacturaTaula_${$("#numFactura").val()}">${$("#numFactura").val()}</td>
             <td>${$("#data").val()}</td>
             <td>${$("#nif").val()}</td>
             <td>${$("#nom").val()}</td>
@@ -154,23 +161,53 @@ $("#guardarFactura").on("click", function(){
     }
 });
 
-function mostrarNumFactura(){
-    $("#numFactura").val(_numFactura);
-}
+//funcion para guardar todos las facturas en un archivo .json on el formato factures.json y quitandole los espacios y el formato de moneda
+$("#guardar").on("click", function(){
+    var facturas = [];
+    var tabla = document.getElementById('taula');
+    var filas = tabla.getElementsByTagName('tr');
 
-function sumarNumFactura(){
-    _numFactura += 1;
-}
+    // Empezamos desde 1 para evitar la fila de encabezados
+    for (var i = 1; i < filas.length; i++) {
+        var fila = filas[i];
+        var celdas = fila.getElementsByTagName('td');
 
-function verificarNumFactura(){
-    let num = $("#numFactura").val();
-    if(num > $("#idTaula").val()){
-        _numFactura = num;
-        _numFactura += 1;
-    }else {
-        $("#numFactura").val(_numFactura);
+        var factura = {
+            "id": celdas[0].innerText,
+            "data": celdas[1].innerText,
+            "nif": celdas[2].innerText,
+            "nom_client": celdas[3].innerText,
+            "telefon": celdas[4].innerText,
+            "email": celdas[5].innerText,
+            "subtotal": parseFloat(celdas[6].innerText),
+            "dte": parseFloat(celdas[7].innerText),
+            "baseI": parseFloat(celdas[8].innerText),
+            "iva": parseFloat(celdas[9].innerText),
+            "total": parseFloat(celdas[10].innerText),
+            "pagada": celdas[11].innerText === "Sí" ? true : false,
+            "articles": [] // Los artículos se pueden agregar posteriormente si se tienen en la tabla
+        };
+
+        facturas.push(factura);
     }
-}
+
+    // Objeto JSON que contiene las facturas
+    var datosJSON = {
+        "factures": facturas
+    };
+
+    // Convertir el objeto JSON a una cadena
+    var datosJSONCadena = JSON.stringify(datosJSON, null, 2);
+
+    // Crear un enlace de descarga para el archivo JSON
+    var enlaceDescarga = document.createElement('a');
+    enlaceDescarga.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(datosJSONCadena));
+    enlaceDescarga.setAttribute('download', 'facturas.json');
+    enlaceDescarga.style.display = 'none';
+    document.body.appendChild(enlaceDescarga);
+    enlaceDescarga.click();
+    document.body.removeChild(enlaceDescarga);
+});
 
 
 $(document).ready(init);
